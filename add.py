32e29c8d24c8afa62f2a28e2f041cb368c7c268c
Pedro Henrique dos Santos Manfrim <phsmanfrim@gmail.com>
2026-03-01 20:57:43 -0300

    Adicionei os arquivos base do sistema, separados por funções. Fiz o inicio do chat com a parte de mensagem introdutória. Fluxo completo do sistema de adição ainda em produção.

---
+++ add.py
@@ -0,0 +1,21 @@
+from telebot import types
+
+chat_data = {}
+
+def first_quest(message, finance_bot):
+  chat_id = message.chat.id
+  chat_data[chat_id] = {}
+
+  markup = types.InlineKeyboardMarkup(row_width=2)
+  markup.add(
+    types.InlineKeyboardButton('Ganhei Dinheiro', callback_data='Ganho'),
+    types.InlineKeyboardButton('Gastei Dinheiro', callback_data='Gasto'),
+  )
+
+  finance_bot.send_message(chat_id, '**Novo registro!!** \n O que você fez com seu dinheiro dessa vez?', reply_markup=markup, parse_mode='Markdown')
+
+## Gain Format
+
+def second_quest_gain(call, finance_bot):
+  chat_id = call.message.chat.id
+  chat_data[chat_id]['entry_type'] = 
---
+++ data_base.py
@@ -0,0 +1,13 @@
+import psycopg2
+
+def add_ganhos():
+  pass
+
+def add_gastos():
+  pass
+
+def get_last():
+  pass
+
+def delete():
+  pass
