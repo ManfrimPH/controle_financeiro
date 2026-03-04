c2f384a54f4fa97ed4c45b7fba5d595c131d36cc
Pedro Henrique dos Santos Manfrim <141989205+ManfrimPH@users.noreply.github.com>
2026-03-04 16:30:06 -0300

    Adição das perguntas de gastos. O fluxo é parecido com a de ganho, porem com uma mensagem a mais do que a de ganhos.

--- add.py
+++ add.py
@@ -9,8 +9,8 @@ def first_quest(message, finance_bot):
 
     markup = types.InlineKeyboardMarkup(row_width=2)
     markup.add(
-        types.InlineKeyboardButton("Ganhei Dinheiro", callback_data="Ganho"),
-        types.InlineKeyboardButton("Gastei Dinheiro", callback_data="Gasto"),
+        types.InlineKeyboardButton("Ganhei Dinheiro", callback_data="gain_entry_type_Ganho"),
+        types.InlineKeyboardButton("Gastei Dinheiro", callback_data="spent_entry_type_Gasto"),
     )
 
     finance_bot.send_message(
@@ -26,20 +26,20 @@ def first_quest(message, finance_bot):
 
 def second_quest_gain(call, finance_bot):
     chat_id = call.message.chat.id
-    chat_data[chat_id]["entry_type"] = call.data
+    chat_data[chat_id]["entry_type"] = call.data.replace('gain_entry_type_', '')
 
     markup = types.InlineKeyboardMarkup()
     markup.add(
-        types.InlineKeyboardButton("Salario", callback_data="gain_entry_type_Salario"),
-        types.InlineKeyboardButton("VR/VA", callback_data="gain_entry_type_VR/VA"),
+        types.InlineKeyboardButton("Gastos Fixos", callback_data="gain_category_Gastos Fixos"),
+        types.InlineKeyboardButton("Estilo de Vida", callback_data="gain_category_Estilo de Vida"),
         types.InlineKeyboardButton(
-            "Renda Extra", callback_data="gain_entry_type_Renda Extra"
+            "Investimento e Projetos", callback_data="gain_category_Investimento e Projetos"
         ),
         types.InlineKeyboardButton(
-            "Investimento", callback_data="gain_entry_type_Investimento"
+            "Besteiras", callback_data="gain_category_Besteiras"
         ),
         types.InlineKeyboardButton(
-            "Presente", callback_data="gain_entry_type_Presente"
+            "Presente", callback_data="gain_category_Presente"
         ),
     )
 
@@ -53,7 +53,7 @@ def second_quest_gain(call, finance_bot):
 
 def third_quest_gain(call, finance_bot):
     chat_id = call.message.chat.id
-    chat_data[chat_id]["category"] = call.data.replace("gain_entry_type_", "")
+    chat_data[chat_id]["category"] = call.data.replace("gain_category_", "")
 
     msg = finance_bot.edit_message_text(
         f"Categoria: *{chat_data[chat_id]['category']}*\n\n **Qual o valor**",
@@ -85,6 +85,101 @@ def salvar_final_financeiro(message,finance_bot):
     resumo = (
         f"**Novo Registrado!**\n\n"
         f"**Tipo de entrada:** {dados['entry_type']}\n\n"
+        f"**Categoria:** {dados['category']}\n"
+        f"**Descrição:** {dados['description']}\n"
+        f"**Valor:** R$ {dados['value']}\n"
+        f"*Dados prontos para o banco de dados!*"
+    )
+    
+    finance_bot.send_message(chat_id, resumo, parse_mode="Markdown")
+    finance_bot.send_message(chat_id, "Dados inseridos!", parse_mode="Markdown")
+
+
+## Spent Format
+
+
+def second_quest_spent(call, finance_bot):
+    chat_id = call.message.chat.id
+    chat_data[chat_id]["entry_type"] = call.data.replace('spent_entry_type_', '')
+
+    markup = types.InlineKeyboardMarkup()
+    markup.add(
+        types.InlineKeyboardButton("Gastos Fixos", callback_data="spent_category_Gastos Fixos"),
+        types.InlineKeyboardButton(
+            "Renda Extra", callback_data="spent_category_Renda Extra"
+        ),
+        types.InlineKeyboardButton(
+            "Investimento", callback_data="spent_category_Investimento"
+        ),
+        types.InlineKeyboardButton(
+            "Lazer", callback_data="spent_category_Lazer"
+        ),
+    )
+
+    finance_bot.send_message(
+        chat_id,
+        "**Com que tipo de coisas você gastou esse dinheiro?**",
+        reply_markup=markup,
+        parse_mode="Markdown",
+    )
+
+
+def third_quest_spent(call, finance_bot):
+    chat_id = call.message.chat.id
+    chat_data[chat_id]["category"] = call.data.replace("spent_category_", "")
+
+    markup = types.InlineKeyboardMarkup()
+    markup.add(
+        types.InlineKeyboardButton("Debito", callback_data="spent_payment_method_Debito"),
+        types.InlineKeyboardButton("VR/VA", callback_data="spent_payment_method_VR/VA"),
+        types.InlineKeyboardButton(
+            "Credito", callback_data="spent_payment_method_Credito"
+        ),
+    )
+
+    finance_bot.send_message(
+        chat_id,
+        "**Como foi que você pagou por isso?**",
+        reply_markup=markup,
+        parse_mode="Markdown",
+    )
+
+
+def fourth_quest_spent(call, finance_bot):
+    chat_id = call.message.chat.id
+    chat_data[chat_id]["payment_method"] = call.data.replace("spent_payment_method_", "")
+
+    msg = finance_bot.edit_message_text(
+        f"Categoria: *{chat_data[chat_id]['category']}*\n\n Categoria: *{chat_data[chat_id]['payment_method']}*\n\n **Qual o valor?**",
+        chat_id,
+        call.message.message_id,
+        parse_mode="Markdown",
+    )
+    
+    finance_bot.register_next_step_handler(msg, fifth_quest_spent, finance_bot)
+
+def fifth_quest_spent(message, finance_bot):
+    chat_id = message.chat.id
+    chat_data[chat_id]["value"] = message.text.replace(",",".")
+
+    msg = finance_bot.send_message(
+        chat_id,
+        f"Categoria: *{chat_data[chat_id]['category']}*\n\n Categoria: *{chat_data[chat_id]['payment_method']}*\n\n *Valor: {chat_data[chat_id]['value']}* \n\n**No que foi esse gasto? (texto livre)**",
+        parse_mode="Markdown",
+    )
+    
+    finance_bot.register_next_step_handler(msg, salvar_final_financeiro, finance_bot)
+
+def salvar_final_financeiro(message,finance_bot):
+    chat_id = message.chat.id
+    chat_data[chat_id]['description'] = message.text
+    
+    dados = chat_data[chat_id]
+    
+    resumo = (
+        f"**Novo Registrado!**\n\n"
+        f"**Tipo de entrada:** {dados['entry_type']}\n\n"
+        f"**Método de pagamento:** {dados['payment_method']}\n"
         f"**Categoria:** {dados['category']}\n"
         f"**Descrição:** {dados['description']}\n"
         f"**Valor:** R$ {dados['value']}\n"
