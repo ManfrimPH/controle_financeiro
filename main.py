27dc4b1d31e410a445389842211cbcb73c92982f
Pedro Henrique dos Santos Manfrim <141989205+ManfrimPH@users.noreply.github.com>
2026-03-04 16:32:36 -0300

    Ajustando imports das funções no main. Adicionando também suas funções em respectivas chamadas de callback.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 import os
 from dotenv import load_dotenv
-from add import first_quest, second_quest_gain, third_quest_gain
+from add import first_quest, second_quest_gain, third_quest_gain,second_quest_spent,third_quest_spent,fourth_quest_spent
 
 load_dotenv()
 bot_key = os.getenv("BOT_KEY")
@@ -17,20 +17,27 @@ def add_command(message):
 ## Gain quests
 
 
-@finance_bot.callback_query_handler(func=lambda call: call.data.startswith("Ganho"))
-def add_callback_one(call):
+@finance_bot.callback_query_handler(func=lambda call: call.data.startswith("gain_entry_type_"))
+def add_callback_gain_one(call):
     second_quest_gain(call, finance_bot)
 
-@finance_bot.callback_query_handler(func=lambda call: call.data.startswith("gain_entry_type_"))
-def add_callback_two(call):
+@finance_bot.callback_query_handler(func=lambda call: call.data.startswith("gain_category_"))
+def add_callback_gain_two(call):
     third_quest_gain(call, finance_bot)
 
 ## Spent quests
 
+@finance_bot.callback_query_handler(func=lambda call: call.data.startswith("spent_entry_type_"))
+def add_callback_spent_one(call):
+    second_quest_spent(call, finance_bot)
 
-# @finance_bot.callback_query_handler(func=lambda call: call.data.startswith("Gasto"))
-# def add_callback_one(call):
-#     second_quest_spent(call, finance_bot)
+@finance_bot.callback_query_handler(func=lambda call: call.data.startswith("spent_category_"))
+def add_callback_spent_two(call):
+    third_quest_spent(call, finance_bot)
+
+@finance_bot.callback_query_handler(func=lambda call: call.data.startswith("spent_payment_method_"))
+def add_callback_spent_three(call):
+    fourth_quest_spent(call, finance_bot)
 
 
 finance_bot.infinity_polling()
